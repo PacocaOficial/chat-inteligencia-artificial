82942042a8b207a0d0807e2066d85c24dbc562ea
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-06 11:44:44 -0300

    feat: especificar melhor conteudo para ser removido

--- main.py
+++ main.py
@@ -40,8 +40,11 @@ async def analyze_post(request: PostRequest):
                     "content": (
                         "Você é um sistema de moderação de conteúdo para a plataforma/rede social Paçoca. "
                         f"Analise o seguinte post com base em nossas diretrizes de uso e moderação. Diretrizes em: {os.getenv('GUIDELINES_URL')}. Termos de uso em: {os.getenv('TERMS_USE_URL')}"
+                        "Se não for tiver informações suficience, responda apenas com: 'Post Permitido''. "
                         "Se o post for permitido, responda apenas com: 'Post Permitido'. "
-                        "Se não for permitido, responda apenas com o motivo da remoção, de forma objetiva e breve."
+                        "Posts contendo opiniões, linguagem informal ou gírias comuns **não devem ser removidos**. "
+                        "Se não for permitido, responda com: 'Post Não Permitido' no inicio da frase e o motivo da remoção, de forma objetiva e breve."
+                        f"O conteúdo do post é: {request.content}"
                     )
                 },
                 {"role": "user", "content": request.content},
@@ -52,7 +55,7 @@ async def analyze_post(request: PostRequest):
 
         ai_message = response["message"]["content"].strip().lower()
 
-        if "post permitido" in ai_message:
+        if "Post Não Permitido" not in ai_message:
             return {"status": "permitido", "analysis": ai_message, "reason": None}
         else:
             return {"status": "removido", "analysis": ai_message, "reason": ai_message}
