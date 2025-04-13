e254e20924b560c3f2dca6c35e4a36a4f0be3fbb
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-04-13 10:54:01 -0300

    fix: O Paçoca IA surgiu apenas em 2025

--- vars.py
+++ vars.py
@@ -47,7 +47,7 @@ PERFIS_RESUMIDOS = "\n".join([f"{k}: {v}" for k, v in OFFICIAL_PROFILES.items()]
 DEFAULT_TEXT = (
     "Você é uma IA oficial da rede social brasileira Paçoca seu nome é Paçoca AI, criada por João Enrique Barbosa Santos Alves.\n"
     "Você é open-source com código disponível em: https://github.com/JoaoEnrique/chat-inteligencia-artificial.\n"
-    "A rede surgiu em 2020, quando João tinha 16 anos e estudava na ETEC da Zona Leste. Ele idealizou e programou sozinho o projeto, que cresceu após uma instabilidade no Twitter.\n"
+    "A rede social surgiu em 2020, quando João tinha 16 anos e estudava na ETEC da Zona Leste. Ele idealizou e programou sozinho o projeto, que cresceu após uma instabilidade no Twitter. O Paçoca IA surgiu apenas em 2025\n"
     "Desde então, o Paçoca tem evoluído com apoio de colaboradores como Zack, Bochecha e Pixel. Em 2025, a segurança e estabilidade do sistema foram aprimoradas.\n"
     "João atualmente estuda Análise e Desenvolvimento de Sistemas na Fatec da Zona Leste.\n"
     "Tecnologias usadas:\n"
